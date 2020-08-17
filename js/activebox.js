4d847f220c517a36dd7cb37524c43463ed5501d2
Ivan <zhuravlev.ivan.vadimovich@gmail.com>
2020-08-17 13:29:06 +0300

    AB

---
+++ js/activebox.js
@@ -0,0 +1,93 @@
+
+$(function() {
+
+	/* ===== Fixed Header ===== */
+	let header = $("#header");
+	let intro = $("#intro");
+	let introH = intro.innerHeight();
+	let scrollPos = $(window).scrollTop();
+	let nav = $("#nav");
+	let navToggle = $("#navToggle");
+
+	checkSkroll(scrollPos, introH);	
+
+
+	$(window).on("scroll resize", function() {
+		introH = intro.innerHeight();
+
+		scrollPos = $(this).scrollTop();
+
+		checkSkroll(scrollPos, introH);	
+	});
+
+	function checkSkroll(scrollPos, introH) {
+		if(scrollPos > introH) {
+			header.addClass("fixed");
+		} else {
+			header.removeClass("fixed");
+		}
+	} 
+
+	/* ===== Smooth scroll ===== */
+	/*Атрибут указывается в квадратных скобках*/
+
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		let elementId = $(this).data("scroll");
+		let elementOffset = $(elementId).offset().top;
+
+		nav.removeClass("show");
+
+		$("html, body").animate({
+			scrollTop: elementOffset -70
+		}, 700);
+	});
+
+
+	/* ===== Nav Toggle ===== */
+
+	$("#navToggle").on("click", function (event) {
+		event.preventDefault();
+		nav.toggleClass("show");
+
+	});
+
+
+	/* ===== Reviews: https://kenwheeler.github.io/slick/ ===== */
+
+	let slider = $("#reviewsSlider");
+
+	slider.slick({
+	  infinite: true,
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  fade: true,
+	  arrows: false,
+	  dots: true
+	});
+
+
+
+
+
+
+
+
+
+/*
+	$("[data-scroll]").on("click", function (event) {
+		event.preventDefault();
+
+		let elementId = $(this).data("scroll");
+		let elementOffset = $(elementId).offset().top;
+
+		console.log (elementOffset);
+
+		$("html, body").animate({
+			scrollTop: elementOffset - 70
+		}, 700);
+	});
+*/
+
+});
